3f41463e6cfe529dc66df6b0c89a6d67c6786664
Kevin Meziere <kmeziere@jpcatholic.com>
2016-03-17 09:57:55 -0700

    build zone WIP

--- snarl-install.sh
+++ snarl-install.sh
@@ -7,8 +7,8 @@ export PATH
 /usr/bin/curl https://project-fifo.net/fifo.gpg > /root/fifo.gpg
 /usr/bin/gpg --primary-keyring /opt/local/etc/gnupg/pkgsrc.gpg --import < /root/fifo.gpg
 /usr/bin/echo "http://release.project-fifo.net/pkg/rel" >> /opt/local/etc/pkgin/repositories.conf
-# /opt/local/bin/pkgin -fy up
-# /opt/local/bin/pkgin -y install fifo-snarl
-# /opt/local/bin/pkgin -yf ug
+/opt/local/bin/pkgin -fy up
+/opt/local/bin/pkgin -y install fifo-snarl
+/opt/local/bin/pkgin -yf ug
 
 /opt/local/bin/sm-prepare-image -y
