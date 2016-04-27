db920b78876277642702b996b870b51a1b617886
Kevin Meziere <kmeziere@jpcatholic.com>
2016-04-27 01:51:32 -0700

    s3 upload script

--- make_all.sh
+++ make_all.sh
@@ -1,4 +1,18 @@
-curl -k https://raw.githubusercontent.com/kevinmeziere/breeder/0.8.0/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "aio" "0.8.0"  "DHCP"
-curl -k https://raw.githubusercontent.com/kevinmeziere/breeder/0.8.0/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "howl" "0.8.0"  "DHCP"
-curl -k https://raw.githubusercontent.com/kevinmeziere/breeder/0.8.0/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "snarl" "0.8.0"  "DHCP"
-curl -k https://raw.githubusercontent.com/kevinmeziere/breeder/0.8.0/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "sniffle" "0.8.0"  "DHCP"
+VER="0.8.0"
+
+cd /var/tmp
+curl -k -O https://raw.githubusercontent.com/kevinmeziere/breeder/${VER}/s3.sh
+chmod +x s3.sh
+
+curl -k -O https://raw.githubusercontent.com/kevinmeziere/breeder/${VER}/dsindex.sh
+chmod +x dsindex.sh
+
+for PKG in aio howl snarl sniffle do
+	curl -k https://raw.githubusercontent.com/kevinmeziere/breeder/${VER}/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "${PKG}" "${VER}"  "DHCP"
+	UUID=$(json -f fifo-${PKG}-${VER}.imgmanifest uuid)
+	./s3.sh fifo-aio-0.8.0.imgmanifest fifo-aio-0.8.0.imgmanifest "application/xml"
+	./s3.sh fifo-aio-0.8.0.zfs.bz2 ${UUID}/file.bz2 "application/x-bzip2"
+done
+
+./dsindex.sh > dsindex
+./s3.sh dsindex dsindex "application/json"
