38b473dcb4f1472a7a20cd4c11c148b1d73087b0
Kevin Meziere <kevinmeziere@Kevins-MacBook-Pro.local>
2017-01-28 22:13:32 -0800

    dsindex no handled at server

--- make_all.sh
+++ make_all.sh
@@ -3,9 +3,6 @@ VER="0.9.0"
 
 cd /var/tmp
 
-curl -s -k -O https://raw.githubusercontent.com/project-fifo/breeder/${VER}/dsindex.sh
-chmod +x dsindex.sh
-
 for PKG in aio howl snarl sniffle 
 do
 	curl -k https://raw.githubusercontent.com/project-fifo/breeder/${VER}/create-zone.sh?$(date -R | awk '{print $5}' | sed 's/\://g') | bash -s "${PKG}" "${VER}"  "DHCP"
